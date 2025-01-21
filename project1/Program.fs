3620f8d0e232c91285978f2e51a068ce209c7f9f
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2025-01-21 16:30:14 +0100

    example

---
+++ project1/Program.fs
@@ -0,0 +1,14 @@
+﻿module Foo
+
+let add1 x = x + 1
+
+let add1Array = Array.map add1
+let add1ArrayWithArg a = a|> Array.map add1
+
+let a = [|1; 2; 3|]
+printfn "Program 1 with reference"
+printfn "Non Point Free: %A" (add1ArrayWithArg a)
+printfn "Point free: %A" (add1Array a)
+
+
+
---
+++ project2/Program.fs
@@ -0,0 +1,5 @@
+﻿open Foo 
+let a = [|8; 9; 3|]
+printfn "Program 2 with reference"
+printfn "Non Point Free: %A" (add1ArrayWithArg a)
+printfn "Point free: %A" (add1Array a)
