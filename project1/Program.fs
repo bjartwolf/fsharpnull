128c0355d5fc858c96ca2f4b641fd885e6faa58e
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2025-01-21 21:26:30 +0100

    console app without entry point

--- project1/Program.fs
+++ project1/Program.fs
@@ -5,3 +5,4 @@ let add1 x = x + 1
 let add1Array = Array.map add1
 let add1ArrayWithArg a = a |> Array.map add1
 
+// no entry point
--- project2/Program.fs
+++ project2/Program.fs
@@ -1,10 +1,11 @@
 ﻿let a = [|8; 9; 3|]
 open FooLib 
-printfn "Refering module from program 3 which is lib"
+printfn "Refering module from program 3 which is console app with entry point"
 printfn "Non Point Free: %A" (add1ArrayWithArg a)
 printfn "Point free: %A" (add1Array a)
 
 open Foo 
-printfn "Refering module from program 1 which is exe"
+
+printfn "Refering module from program 1 which is console app withOUT entry point"
 printfn "Non Point Free: %A" (add1ArrayWithArg a)
 printfn "Point free: %A" (add1Array a)
